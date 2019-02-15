8eebb0b64bd6e084dda7b0f1ebd788e22b5b43d9
홍태환(DavidTaehwan Hong)/HR팀/11ST <taehwan.hong@sk.com>
2019-02-15 11:34:13 +0900

    create a list for todos

--- src/components/TodoPages.vue
+++ src/components/TodoPages.vue
@@ -1,41 +1,54 @@
 <template>
-  <div class="container">
-    <h2> 구매희망 도서 신청 </h2>
-    <div class="input-group" style="margin-bottom:10px;">
-      <input type="text" class="form-control" placeholder="희망하는 도서의 제목을 입력하세요">
-      <span class="input-group-btn">
-        <button class="btn btn-default" type="button"> 신청 </button>
-      </span>
-    </div>
-    <ul class="list-group">
-      <li class="list-group-item">
-        구매 희망 도서 Lists
-        <div class="btn-group pull-right" style="font-size: 12px line-height: 1;">
-            <button type="button" class="btn-link dropdown-toggle"
-              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              더보기 <span class="caret"></span>
-            </button>
-            <ul class="dropdown-menu">
-              <li>
-                <a href="#">삭제</a>
-              </li>
-            </ul>
-        </div>
-      </li>
-    </ul>
+<div class="container">
+  <h2>구매 희망 도서</h2>
+  <div class="input-group" style="margin-bottom:10px;">
+    <input type="text" class="form-control" placeholder="도서 명을 입력하세요">
+    <span class="input-group-btn" >
+      <button class="btn btn-default" type="button">추가</button>
+    </span>
   </div>
+  <ul class="list-group">
+    <li class="list-group-item" v-for="(todo, index) in todos">
+    {{todo.name}}
+      <div class="btn-group pull-right" 
+        style="font-size: 12px; line-height: 1;">
+        <button type="button" 
+        class="btn-link dropdown-toggle" 
+        data-toggle="dropdown" 
+        aria-haspopup="true" 
+        aria-expanded="false">
+          더보기 <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu">
+          <li><a href="#">삭제</a></li>
+          <li><a href="#">변경</a></li>
+        </ul>
+      </div>
+    </li>
+  </ul>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'ToDoPages',
+  name: 'TodoPage',
   data() {
     return {
-      msg: 'welcome. Todo page',
+      todos: [
+        {
+          name: '신청도서 1',
+        },
+        {
+          name: '신청도서 2',
+        },
+        {
+          name: '신청도서 3',
+        },
+        {
+          name: '신청도서 4',
+        },
+      ],
     };
-  },
-  mounted() {
-    console.log('component has mounted');
   },
 };
 </script>
